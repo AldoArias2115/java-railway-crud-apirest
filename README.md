abe9b4b006be60912520862591b7aabf6977302e
AldoArias2115 <aldo_097@hotmail.com>
2024-08-29 10:56:38 -0600

    Primer commit de de Version Modificada para desplegar en railway

---
+++ README.md
@@ -0,0 +1,2 @@
+# java-crud-api-rest
+CRUD Basico en JAVA atreves de un API REST utilizando Spring boot, Hibernate, PostgreSQL
---
+++ src/main/java/com/aldo/apirest/apirest/Repositories/ProductoRepository.java
@@ -0,0 +1,9 @@
+package com.aldo.apirest.apirest.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.aldo.apirest.apirest.Entities.Producto;
+
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
+
+}
